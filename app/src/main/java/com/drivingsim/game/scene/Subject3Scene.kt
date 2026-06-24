3f00600db8f920426e312bacac77e4afd9cc1586
zhco <luycheung@qq.com>
2026-06-24 20:27:31 +0800

    fix

--- app/src/main/java/com/drivingsim/game/scene/Subject3Scene.kt
+++ app/src/main/java/com/drivingsim/game/scene/Subject3Scene.kt
@@ -76,7 +76,7 @@ class Subject3Scene : Scene() {
     override fun isComplete(): Boolean = false
 
     override fun checkCollision(vehicle: Vehicle): CollisionResult =
-        CollisionResult(false, false)
+        CollisionResult(false, false, false)
 
     override fun reset() {}
 }
